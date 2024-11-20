a4d6efc92480881522982ae62b8511d6cc14d2d4
mohamed aziz braham <mohamed_aziz.braham@etud.univ-jfc.fr>
2024-11-20 09:29:01 +0100

    Modification de la class UE

--- src/main/java/champollion/UE.java
+++ src/main/java/champollion/UE.java
@@ -1,15 +1,22 @@
 package champollion;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class UE {
     private final String myIntitule;
 
-    public UE(String intitule) {
-        myIntitule = intitule;
+    private int heureCM;
+    private int heureTD;
+    private int heureTP;
+    private Set<Intervention> interventions = new HashSet<>();
+
+    public UE(String myIntitule) {
+        this.myIntitule = myIntitule;
     }
 
     public String getIntitule() {
         return myIntitule;
     }
 
-    
 }
